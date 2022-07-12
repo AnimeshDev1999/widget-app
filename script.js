349b61e733f659f852740b8f27811542b7194659
Animesh <animesh99dev@gmail.com>
2022-07-12 23:19:35 +0530

    finished the app

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const clockEl = document.querySelector(".clock");
 const timeOfDay = document.querySelector(".tod");
 const curDate = document.querySelector(".date");
 const widget = document.querySelector(".wid");
+const greetEl = document.querySelector(".greet");
 const yearMonths = [
   "Jan",
   "Feb",
@@ -25,6 +26,7 @@ let date = new Date();
 function ampm() {
   clockEl.classList.toggle("cur");
   clockEl.classList.toggle("glo");
+  document.querySelector(".b1").classList.toggle("one");
 }
 
 function curDates() {
@@ -39,6 +41,15 @@ function curTime() {
   let hour = new Date().getHours();
   let min = new Date().getMinutes();
   let tod = "AM";
+  let greet = "";
+
+  if (hour <= 12) {
+    greet = "Morning";
+  } else if (hour > 12 && hour <= 18) {
+    greet = "Afternoon";
+  } else {
+    greet = "Evening";
+  }
 
   if (hour >= 12) {
     tod = "PM";
@@ -50,6 +61,13 @@ function curTime() {
     hour = hour % 12;
   }
 
+  if (greet == "Evening") {
+    document.querySelector(".ico").setAttribute("name", "moon");
+  } else {
+    document.querySelector(".ico").setAttribute("name", "sunny");
+  }
+
+  greetEl.textContent = `Good ${greet}`;
   clockEl.innerText = `${hour}:${min}`;
   timeOfDay.innerText = tod;
 }
@@ -62,6 +80,12 @@ function themeSw() {
     widget.classList.remove("th2");
     widget.classList.add("th1");
   }
+  document.querySelector(".b2").classList.toggle("two");
+}
+
+function darkLight() {
+  document.querySelector(".mid").classList.toggle("light");
+  document.querySelector(".b3").classList.toggle("three");
 }
 
 curDates();
